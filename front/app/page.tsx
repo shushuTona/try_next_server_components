db72a0098c70f51a76699d8e3b1ec65f6d6340ad
shushuTona <42406967+shushuTona@users.noreply.github.com>
2024-12-28 21:22:16 +0900

    create pages

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,26 +1,20 @@
-import Counter from "@/client/counter";
-import Token from "@/client/token";
-import Link from "next/link";
+import LinkList from "@/server/linklist";
+import Login from "@/client/login";
+import { checkLogin } from "../auth/login";
 
-const getTokne = async () => {
-    const res = await fetch('http://server:8001/gettoken', {cache: 'no-store'})
-    return res.json();
-}
-
-type GetToken = {
-    token: string
-}
-
-const Home = async ()=> {
-    const gettoken: GetToken = await getTokne();
-    console.log(gettoken.token);
+const Home = async () => {
+    const res = await checkLogin();
+    const isLogin = res.ok;
 
     return (
         <main className="text-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <h1>Home</h1>
-            <Token token={gettoken.token} />
-            <Counter title="カウンター" />
-            <Link href="/other">Other</Link>
+            {isLogin ? (
+                <p>ログインしています。</p>
+            ) : (
+                <Login />
+            )}
+            <LinkList />
         </main>
     );
 }
---
+++ front/app/page1/page.tsx
@@ -0,0 +1,19 @@
+import LinkList from "@/server/linklist";
+import { checkLogin } from "../../auth/login";
+import { redirect } from "next/navigation";
+
+const Page1 = async (  ) => {
+    const res = await checkLogin();
+    if ( !res.ok ) {
+        redirect( "/" );
+    }
+
+    return (
+        <main className="text-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <h1>Page1</h1>
+            <LinkList />
+        </main>
+    )
+}
+
+export default Page1;
